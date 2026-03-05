cca208d6eed04de94456e84d0e3635da55e424fc
dwesh163 <dwesh163@protonmail.com>
2026-03-05 09:49:53 +0100

    [update] bump version to 2.1.0 for plugin release

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Custom Wheel
  * Description:     The plugin adds an interactive spinning wheel with prize management.
- * Version:         2.0.0
+ * Version:         2.1.0
  * Author:          CEA Informatics
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
